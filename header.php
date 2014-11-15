9d6f1a4d538b74f21ce04431ad4435e4f6857297
TirtaWR <tirtawening@gmail.com>
2014-11-15 23:14:54 +0700

    kalender otw, to do list mau dikerjain

---
+++ header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+
+
+
+<head>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="description" content="Deskripsi Blog">
+<meta name="author" content="Judul Blog">
+
+<!-- Twitter Card -->
+<meta name="twitter:card" content="summary">
+<meta name="twitter:site" content="omfgitsasalmon">
+<meta name="twitter:title" content="Simple Blog">
+<meta name="twitter:description" content="Deskripsi Blog">
+<meta name="twitter:creator" content="Simple Blog">
+<meta name="twitter:image:src" content="{{! TODO: ADD GRAVATAR URL HERE }}">
+
+<meta property="og:type" content="article">
+<meta property="og:title" content="Simple Blog">
+<meta property="og:description" content="Deskripsi Blog">
+<meta property="og:image" content="{{! TODO: ADD GRAVATAR URL HERE }}">
+<meta property="og:site_name" content="Simple Blog">
+
+<link rel="stylesheet" type="text/css" href="assets/css/screen.css" />
+<link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+
+<!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<title>Tirta's Simple Blog</title>
+
+
+<link href='calendar/fullcalendar.css' rel='stylesheet' />
+<link href='calendar/fullcalendar.print.css' rel='stylesheet' media='print' />
+<script src='calendar/lib/moment.min.js'></script>
+<script src='calendar/lib/jquery.min.js'></script>
+<script src='calendar/fullcalendar.min.js'></script>
+
+
+</head>
