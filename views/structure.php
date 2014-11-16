7ce926453f0388616102f84104af7b6497f86218
andresusanto <andre@andresusanto.info>
2014-11-16 17:28:03 +0700

    Bagian panitia versi 1

---
+++ views/structure.php
@@ -0,0 +1,48 @@
+		
+		<div class="page-header">
+          <div class="row">
+            <div class="col-lg-12">
+              <h1 id="member">Pasar Seni 2014</h1>
+            </div>
+          </div>
+        </div>
+		
+        <!-- Title -->
+        <div class="row">
+			
+            <div class="col-lg-12">
+				
+
+				<ul class="nav nav-tabs">
+				  <li class=""><a href="page/committee">Overview</a></li>
+				  <li class="active"><a href="javascript:void(0);">Organization Structure</a></li>
+				  <li class="disabled"><a>Disabled</a></li>
+				  <li class="dropdown">
+					<a class="dropdown-toggle" data-toggle="dropdown" href="#">
+					  Dropdown <span class="caret"></span>
+					</a>
+					<ul class="dropdown-menu">
+					  <li><a href="#dropdown1" data-toggle="tab">Action</a></li>
+					  <li class="divider"></li>
+					  <li><a href="#dropdown2" data-toggle="tab">Another action</a></li>
+					</ul>
+				  </li>
+				</ul>
+				<div id="myTabContent" class="tab-content">
+				  <div class="tab-pane fade active in" id="ov">
+					<div id="orgdiagram" style="width: 100%; height: 600px; overflow: hidden; left: 0px; padding: 0px; margin: 0px;"></div>
+				</div>
+				  <div class="tab-pane fade" id="structure">
+					<div id="orgdiagram" style="width: 100%; height: 600px; overflow: hidden; left: 0px; padding: 0px; margin: 0px;"></div>
+					</div>
+				  <div class="tab-pane fade" id="dropdown1">
+					<p>Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro fanny pack lo-fi farm-to-table readymade. Messenger bag gentrify pitchfork tattooed craft beer, iphone skateboard locavore carles etsy salvia banksy hoodie helvetica. DIY synth PBR banksy irony. Leggings gentrify squid 8-bit cred pitchfork.</p>
+				  </div>
+				  <div class="tab-pane fade" id="dropdown2">
+					<p>Trust fund seitan letterpress, keytar raw denim keffiyeh etsy art party before they sold out master cleanse gluten-free squid scenester freegan cosby sweater. Fanny pack portland seitan DIY, art party locavore wolf cliche high life echo park Austin. Cred vinyl keffiyeh DIY salvia PBR, banh mi before they sold out farm-to-table VHS viral locavore cosby sweater.</p>
+				  </div>
+				</div>
+				
+            </div>
+        </div>
+        <!-- /.row -->
